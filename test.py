98f0a351e6a0201f0fe3f0c5120a5d8683b2e31d
芒果糯米糍 <20512058+axhlzy@users.noreply.github.com>
2021-10-15 15:52:14 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
-from Hook import functionsMap, MergeUtils, UnityJumper
+from Hook import MergeUtils, UnityJumper
 
 if __name__ == '__main__':
-    ins = MergeUtils(r"C:\Users\pc\Desktop\temp\26_com.xxxxxx\libil2cpp_cp32.so",
-                     r"C:\Users\xxxxxxxx\libinject.so")
+    ins = MergeUtils(r"C:\xxxxxxx\libil2cpp_cp32.so")
     newSoPath = ins.mergeSection(".inject")
 
-    ins.recordSymbols({"il2cpp_string_new": 0x1BD130, "FindClass": 0xAEA06C, "GetStaticMethodID": 0xAEA9D4,
-                       "CallStaticVoidMethod": 0xAEBDB8})
+    # 如果要用到JNI 这些函数是必填项
+    ins.recordSymbols({"il2cpp_string_new": 0x2BE988, "FindClass": 0xEE5684, "GetStaticMethodID": 0xEE60EC,
+                       "CallStaticVoidMethod": 0xEE74E4})
 
+    # 自己需要的Hook的函数名以及地址（记得recordSymbol在UnityJumper构造之前添加）
     ins.recordSymbol("ShowSettings", 0xB69D4C)
+    ins.recordSymbol("UI_Splash", 0x41c4c8)
 
     ins = UnityJumper(newSoPath)
 
-    ins.addHook(functionsMap.get("ShowSettings"), jmpType="B")
-    ins.android_log_print(msg="中文描述 : called this function")
+    ins.addHook(ins.getSymbolByName("UI_Splash"), printRegs=False)
+    ins.android_log_print_msg(msg="描述 : called this function")
+    # android_log_print_reg 从R3开始,多余的参数使用堆栈传参 prepareStack saveRegToStack restoreStack
+    ins.patchASM("MOV R3,#0x123456")
+    ins.android_log_print_reg()
     ins.CallStaticVoidMethod("com/ironsource/unity/androidbridge/AndroidBridge", "onResume", "()V", 0)
     ins.endHook()
 
